40364301be523735b3f584b395cb3ee9ab1566b4
Arpit Thakkar <arpit.thakkar@green-apex.com>
2015-12-04 19:54:05 +0530

    Format the code

--- StringReverse/src/com/ga/domain/controller/ReverseStringController.java
+++ StringReverse/src/com/ga/domain/controller/ReverseStringController.java
@@ -1,34 +1,26 @@
 package com.ga.domain.controller;
-
-// TODO: Auto-generated Javadoc
 /**
  * The Class ReverseStringController.
+ * 
+ * @author Nilay
  */
 public class ReverseStringController {
-
     /**
      * The main method.
-     * 
-     * @author Nilay
-     *
      * @param args the arguments
      */
     public static void main(String args[]) {
-
         String word = "Band";
         String reverse = reverse(word);
         System.out.printf(" original String : %s , reversed String %s %n", word, reverse);
     }
-
     /**
      * Reverse.
-     *
-     * @author Nilay
+     * Reverse the character of given string
      * @param source the source
      * @return the string
      */
     public static String reverse(String source) {
-
         if (source == null || source.isEmpty()) {
             return source;
         }
@@ -36,8 +28,6 @@ public class ReverseStringController {
         for (int i = source.length() - 1; i >= 0; i--) {
             reverse = reverse + source.charAt(i);
         }
-
         return reverse;
     }
-
 }

82702b2f1fd2149b8ac05cd690cf8eebf7e50550
Arpit Thakkar <arpit.thakkar@green-apex.com>
2015-12-04 19:45:19 +0530

    StringReverse Project added

--- StringReverse/src/com/ga/domain/controller/ReverseStringController.java
+++ StringReverse/src/com/ga/domain/controller/ReverseStringController.java
@@ -1,46 +1,43 @@
-package com.greenapex.gkadmin.admin.domain.controller;
+package com.ga.domain.controller;
 
 // TODO: Auto-generated Javadoc
 /**
- * The Class ReverseController.
+ * The Class ReverseStringController.
  */
-public class ReverseController {
-    
+public class ReverseStringController {
+
     /**
      * The main method.
+     * 
+     * @author Nilay
      *
-     *@author Nilay
      * @param args the arguments
      */
     public static void main(String args[]) {
-    
+
         String word = "Band";
         String reverse = reverse(word);
         System.out.printf(" original String : %s , reversed String %s %n", word, reverse);
-    }   
-  
- 
+    }
+
     /**
      * Reverse.
      *
-     *@author Nilay
+     * @author Nilay
      * @param source the source
      * @return the string
      */
-    public static String reverse(String source){
-        
-        
-        if(source == null || source.isEmpty()){
+    public static String reverse(String source) {
+
+        if (source == null || source.isEmpty()) {
             return source;
-        }       
+        }
         String reverse = "";
-        for(int i = source.length() -1; i>=0; i--){
+        for (int i = source.length() - 1; i >= 0; i--) {
             reverse = reverse + source.charAt(i);
         }
-      
+
         return reverse;
     }
-
-
 
 }
